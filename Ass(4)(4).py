07a1f02599754bdcd681da70b29a30dd40e729dd
BISHAL KUMAR PATRA <msd337788@gmail.com>
2026-08-08 19:36:46 +0530

    Refactor function definition and indentation

--- Ass(4)(4).py
+++ Ass(4)(4).py
@@ -1,10 +1,10 @@
 def fun():
-a=0
-b=1
-print("THE SERIES:",a,b,end=' ')
-for i in range(13):
-    c=a+b
-    a=b
-    b=c
-    print(c,end=' ')    
+    a=0
+    b=1
+    print("THE SERIES:",a,b,end=' ')
+    for i in range(13):
+        c=a+b
+        a=b
+        b=c
+        print(c,end=' ')    
 fun()
